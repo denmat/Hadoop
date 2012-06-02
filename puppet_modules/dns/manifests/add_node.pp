1ff740d94a9877a5854ca00a6579a737c5e410d3
unknown <dennism1@EAPF07BCB15AC6C.apac.experian.local>
2012-06-02 11:10:12 +1000

    changing dns for vagrant hosts

--- puppet_modules/dns/manifests/add_node.pp
+++ puppet_modules/dns/manifests/add_node.pp
@@ -1,19 +1,14 @@
-define dns::add_node {
+class dns::add_node {
 
-  # add the ip address of eth0 to /etc/hosts and restart dnsmasq
-  @@host {$name:
-    ip   => $::ipaddress,
-    host_aliases => ["$::node.$::domain",$::node ],
-  }
+  include dns::dnsmasq::dnsmasq_service
+  
+  $dns_hosts = hiera('dns_hosts')
+  $defaults = { notify => Service['dnsmasq'] }
+  
+  create_resources(host, $dns_hosts, $defaults)
+  
 
-  if $::puppet == 'master' {
-    include dns::dnsmasq::dnsmasq_service
 
-    Host <<| |>> ->
-    exec {"/bin/true":
-      notify => Service['dnsmasq']
-    }
-  }
 }
 
   
